62b5fcb6a4043f526a32c5d0a8ac678fdb0189e3
houdamzari <houda.mzari1999@gmail.com>
2023-04-14 01:35:10 +0000

    Update store

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { persistor, store } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <React.StrictMode>s
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
