f1647cb8deba2289476f6549aa5c05f2972ea3ce
houdamzari <houda.mzari1999@gmail.com>
2023-04-14 01:27:25 +0000

    Update Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DragZones, Button } from "./components";
-
+import { TouchBackend } from "react-dnd-touch-backend";
+import { DragZones } from "./components";
+import { useDispatch } from "react-redux";
+import { addElementFirstZone, resetSecondZone } from "./reducer/inputsSlice";
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(addElementFirstZone([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]));
+    dispatch(resetSecondZone());
+  }, []);
+  const options = {
+    enableMouseEvents: true,
+  };
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={TouchBackend} options={options}>
       <DragZones />
     </DndProvider>
   );
