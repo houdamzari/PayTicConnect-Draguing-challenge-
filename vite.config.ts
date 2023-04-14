4d65971c3ea3da091d8b323e9c0837f300fcc99e
HOUDA MZARI <73146419+houdamzari@users.noreply.github.com>
2023-04-14 16:01:25 +0000

    Revert "Homepage"

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: "happy-dom",
-    coverage: {
-      provider: "istanbul", // or 'c8',
-      all: true,
-    },
-  },
-});
+  plugins: [react()],
+})
